145e1f985c29e309f428334f6a6e4ddedfb6af57
Alex Momotyuk <o.a.momotyuk@gmail.com>
2020-05-26 23:52:04 -0500

    added Customer class

---
+++ Subscription_Records/Subscription_Records/Models/Customer.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Subscription_Records.Models
+{
+    public class Customer : IdentityUser
+    {
+        [Required]
+        [Display(Name = "User Type")]
+        public int UserType { get; set; }
+
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Required]
+        [Display(Name = "Street Address")]
+        public string StreetAddress { get; set; }
+    }
+}
